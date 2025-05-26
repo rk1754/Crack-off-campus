fdc218a1ba916eb636d5195669533bebe5de5337
Krishnachavan1930 <krishnachavan459@gmail.com>
2025-05-27 00:45:59 +0530

    added job page

--- frontend/src/redux/config.ts
+++ frontend/src/redux/config.ts
@@ -1,8 +1,9 @@
-
 // Use relative path in development and absolute in production
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 // Change from localhost to production URL or relative path
-export const BACKEND_URL = isProduction 
-  ? "http://82.25.105.109:5454/api/v1"
-  : "http://api.crackoffcampus.com:5454/api/v1";
+// export const BACKEND_URL = isProduction
+//   ? "http://82.25.105.109:5454/api/v1"
+//   : "http://api.crackoffcampus.com:5454/api/v1";
+
+export const BACKEND_URL = "http://localhost:5454/api/v1";
