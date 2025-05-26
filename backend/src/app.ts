733d136b42fe3ca79865b1f049b4328404e76f76
Atharva Bhole <atharvabhole239@gmail.com>
2025-05-27 00:03:21 +0530

    Commit resume feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -104,6 +104,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(PORT, async () => {
-  // await sequelize.sync({force : true});
+  // await sequelize.sync({force : false});
   console.log(`Server is running on port ${PORT}`);
 });
