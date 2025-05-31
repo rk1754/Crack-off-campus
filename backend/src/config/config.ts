a045c7644bafc866a06889c3f0356bec548bbddd
Krishnachavan1930 <krishnachavan459@gmail.com>
2025-05-31 20:59:48 +0530

    fixed the issue of job listing page

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -13,24 +13,28 @@ export const DB_HOST: string = process.env.DB_HOST || "";
 
 // Google ID
 export const GOOGLE_CLIENT_ID: string = process.env.GOOGLE_CLIENT_ID || "";
-export const GOOGLE_CLIENT_SECRET: string = process.env.GOOGLE_CLIENT_SECRET || "";
+export const GOOGLE_CLIENT_SECRET: string =
+  process.env.GOOGLE_CLIENT_SECRET || "";
 
 // Session Secret
 export const JWT_SECRET: string = process.env.JWT_SECRET || "changeme";
 export const JWT_EXPIRES_IN: string = process.env.JWT_EXPIRES_IN || "2d";
 
 // Bcrypt Salt
-export const BCRYPT_SALT: number = parseInt(process.env.BCRYPT_SALT as string) || 10;
+export const BCRYPT_SALT: number =
+  parseInt(process.env.BCRYPT_SALT as string) || 10;
 
 // SMTP
 export const SMTP_HOST: string = process.env.SMTP_HOST || "";
-export const SMTP_PORT: number = parseInt(process.env.SMTP_PORT as string) || 465;
+export const SMTP_PORT: number =
+  parseInt(process.env.SMTP_PORT as string) || 465;
 export const SMTP_USER: string = process.env.SMTP_USER || "";
 export const SMTP_PASSWORD: string = process.env.SMTP_PASSWORD || "";
 
 // Razorpay
 export const RAZORPAY_KEY_ID: string = process.env.RAZORPAY_KEY_ID || "";
-export const RAZORPAY_KEY_SECRET: string = process.env.RAZORPAY_KEY_SECRET || "";
+export const RAZORPAY_KEY_SECRET: string =
+  process.env.RAZORPAY_KEY_SECRET || "";
 
 // Cloudinary Config
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
@@ -39,7 +43,8 @@ export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "";
 
 // Frontend URL for CORS and password reset links
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:8080";
-export const FRONTEND_URL_2 = process.env.FRONTEND_URL_2 || "http://crackoffcampus.com";
+export const FRONTEND_URL_2 =
+  process.env.FRONTEND_URL_2 || "http://crackoffcampus.com";
 
 // Cashfree Config
 export const CASHFREE_CLIENT_ID = process.env.CASHFREE_CLIENT_ID;
